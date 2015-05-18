e966af7596d723028aaf3e296b9fed5c24a69b4c
weishai <masterkingr9@gmail.com>
2015-05-19 02:08:00 +0800

    0.70v

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Bling app.js by weishai
 
 var Config = {
+  site: 'http://bling.treedom.cn/',
   API: {
     createSnap: 'http://bling.treedom.cn/ajax/api/create_snap/',
     getSnap: 'http://bling.treedom.cn/ajax/api/get_snap/',
@@ -11,13 +12,13 @@ var Config = {
 
 //debug
 // Config.API.createSnap = '../test/api/create_snap.json'
-Config.API.getSnap = 'test/api/get_snap.json'
+// Config.API.getSnap = 'test/api/get_snap.json'
 
-var Bling = function () {
-  
+var Bling = function() {
+
 }
 
-Bling.prototype.init = function () {
+Bling.prototype.init = function() {
   var self = this
 
   this.data = {}
@@ -25,16 +26,16 @@ Bling.prototype.init = function () {
   this.render()
 }
 
-Bling.prototype.getSnapId = function () {
+Bling.prototype.getSnapId = function() {
   //Todo: get from url
   return getUrlParam('snapimg')
 }
 
-Bling.prototype.showSnap = function (cb) {
-  var self = this
-    , $body = $('body')
-    , snapData = this.data.snap
-    , isHideImg = false
+Bling.prototype.showSnap = function(cb) {
+  var self = this,
+    $body = $('body'),
+    snapData = this.data.snap,
+    isHideImg = false
 
   $body.addClass('bling-start')
     .one('touchstart', function(e) {
@@ -42,12 +43,12 @@ Bling.prototype.showSnap = function (cb) {
 
       showSnapImg(snapData.snapImgUrl, snapData.snapShowTime)
 
-      $body.one('touchend', function (et) {
+      $body.one('touchend', function(et) {
         hideSnapImg(cb)
         et.preventDefault()
       })
 
-      autoHideSet = setTimeout(function(){
+      autoHideSet = setTimeout(function() {
         hideSnapImg(cb)
       }, snapData.snapShowTime * 1000)
 
@@ -58,12 +59,12 @@ Bling.prototype.showSnap = function (cb) {
     $body.addClass('bling-showimg')
       .find('#snapImg').attr('src', url)
 
-    self.countDown(1, time*1000)
+    self.countDown(1, time * 1000)
     return true
   }
 
   function hideSnapImg(cb) {
-    if(isHideImg){
+    if (isHideImg) {
       return
     }
     $body.addClass('bling-hideimg')
@@ -75,116 +76,155 @@ Bling.prototype.showSnap = function (cb) {
   }
 }
 
-Bling.prototype.showSnapLuck = function () {
-  var self = this
-    , snapData = this.data.snap
+Bling.prototype.showSnapLuck = function() {
+  var self = this,
+    snapData = this.data.snap
 
-  if(!snapData.isSnapLuck){
-    return
-  }
+  // if(!snapData.isSnapLuck){
+  //   return
+  // }
 
   $('body').addClass('bling-upload')
 
 }
 
-Bling.prototype.countDown = function (percent, time) {
+Bling.prototype.countDown = function(percent, time) {
   var canvas = document.getElementById('countDown');
   var ctx = canvas.getContext("2d");
   var W = canvas.width;
   var H = canvas.height;
-  var R = H/2.5;
-  var deg=0,new_deg=0,dif=0;
-  var loop,re_loop;
-  var text,text_w;
+  var R = H / 2.5;
+  var deg = 0,
+    new_deg = 0,
+    dif = 0;
+  var loop, re_loop;
+  var text, text_w;
 
   $countDown = $('#countDown')
   $countDown.addClass('countDown-show')
   draw(percent, time)
-  
-  function init(){
+
+  function init() {
     // ctx.clearRect(0,0,W,H);
     // ctx.beginPath();
     // ctx.strokeStyle="rgba(0,0,0,0)";
     // ctx.lineWidth=1;
     // ctx.arc(W/2,H/2,R,0,Math.PI*2,false);
     // ctx.stroke();
-    
-    var r = deg*Math.PI/180;
+
+    var r = deg * Math.PI / 180;
     ctx.beginPath();
     ctx.strokeStyle = "#cba200";
-    ctx.lineWidth=1;
-    ctx.arc(W/2,H/2,R,0-90*Math.PI/180,r-90*Math.PI/180,false);
+    ctx.lineWidth = 1;
+    ctx.arc(W / 2, H / 2, R, 0 - 90 * Math.PI / 180, r - 90 * Math.PI / 180, false);
     ctx.stroke();
-    
+
     // ctx.fillStyle="#cba200";
     // ctx.font="12px";
     // text = Math.floor(deg/360*100)+"%";
     // text_w = ctx.measureText(text).width;
     // ctx.fillText(text,W/2 - text_w/2,H/2+15);
   }
-  function draw(p, t){
+
+  function draw(p, t) {
     //new_deg = Math.round(Math.random()*360);
-    new_deg = Math.round(p*360);
-    dif = new_deg-deg;
-    loop = setInterval(to,t/dif);
+    new_deg = Math.round(p * 360);
+    dif = new_deg - deg;
+    loop = setInterval(to, t / dif);
   }
-  function to(){
-    if(deg == new_deg){
-      clearInterval(loop);
-      $countDown.remove()
+
+  function to() {
+      if (deg == new_deg) {
+        clearInterval(loop);
+        $countDown.remove()
+      }
+      if (deg < new_deg) {
+        deg++;
+      } else {
+        deg--;
+      }
+      init();
     }
-    if(deg<new_deg){
-      deg++;
-    }else{
-      deg--;
-    }
-    init();
-  }
-  //re_loop = setInterval(draw,2000);
+    //re_loop = setInterval(draw,2000);
 }
 
-Bling.prototype.choseImg = function () {
+Bling.prototype.choseImg = function() {
   var self = this
     // , imgId = '../img/test.jpg'
 
+  // alert('choseImg')
+
   wx.chooseImage({
-    success: function (res) {
-      self.data.uploadImgId = res.localIds
+    success: function(res) {
+      // alert('chose img success')
+      self.data.localImgId = res.localIds
       $('.page-upload').addClass('page-upload-ready')
       $('#Preview').attr('src', res.localIds)
     }
   })
 }
 
-Bling.prototype.loading = function (cb) {
-  var self = this
-    , $loading = $('#Loading')
+Bling.prototype.loading = function(cb) {
+  var self = this,
+    $loading = $('#Loading')
   $loading.show()
-  setTimeout(function () {
+  setTimeout(function() {
     $loading.addClass('loading-on')
   }, 0)
   cb && cb($loading)
 }
 
-Bling.prototype.submitImg = function () {
+Bling.prototype.submitImg = function() {
   var self = this
-    , postData = {}
+    , localImgId
 
-  postData.imgId = this.data.uploadImgId
-  if(!postData.imgId){
+  localImgId = self.data.localImgId[0]
+  // alert(localImgId)
+  if (!localImgId) {
     alert('还没选择图片')
+    return false
   }
-  $.get(Config.API.createSnap+postData.imgId, function (res) {
-    if(res.code == 0){
-      alert('提交成功，可以分享了')
-    }
-    else {
-      alert('提交失败，稍后再试')
+  wx.uploadImage({
+    localId: localImgId, // 需要上传的图片的本地ID，由chooseImage接口获得
+    isShowProgressTips: 1, // 默认为1，显示进度提示
+    success: function(res) {
+      var serverId = res.serverId; // 返回图片的服务器端ID
+      $.get(Config.API.createSnap + serverId, function(res) {
+        if (res.code == 0) {
+          self.data.curShareId = serverId
+          self.updateShare()
+          alert('提交成功，可以分享了')
+        } else {
+          alert('提交失败，稍后再试')
+        }
+      })
     }
   })
 }
 
-Bling.prototype.bindEvent = function () {
+Bling.prototype.updateShare = function() {
+  var self = this
+    , shareUrl
+  shareUrl = Config.site + '?snapimg=' + self.data.curShareId
+  wx.onMenuShareAppMessage({
+    title: 'bling', // 分享标题
+    desc: 'bling desc', // 分享描述
+    link: shareUrl, // 分享链接
+    imgUrl: '', // 分享图标
+    // type: '', // 分享类型,music、video或link，不填默认为link
+    // dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+    success: function () {
+        // 用户确认分享后执行的回调函数
+        alert('已分享给朋友')
+    },
+    cancel: function () { 
+      alert('取消分享朋友')
+        // 用户取消分享后执行的回调函数
+    }
+});
+}
+
+Bling.prototype.bindEvent = function() {
   var self = this
 
   $('body')
@@ -193,27 +233,35 @@ Bling.prototype.bindEvent = function () {
       e.preventDefault()
     })
 
-  $('#submitImg').on('touchstart', function (e) {
+  $('#submitImg').on('touchstart', function(e) {
+    // alert('submitImg')
     self.submitImg()
     e.preventDefault()
   })
 
 }
 
-Bling.prototype.render = function () {
-  var self = this
-    , curSnapId = this.getSnapId()
+Bling.prototype.render = function() {
+  var self = this,
+    curSnapId = this.getSnapId()
 
-  // if(!curSnapId){
-  //   return false
-  // }
+  if (!curSnapId) {
+    return false
+  }
 
-  $.get(Config.API.getSnap+curSnapId, function(data) {
+  $.get(Config.API.getSnap + curSnapId, function(data) {
     //debug
     // console.log(data)
     // data = JSON.parse(data)
+    //debug
+    // self.data.snap = {
+    //   snapImgUrl: 'test'
+    // }
+    // self.showSnap()
+    // self.showSnapLuck()
+    // return;
     console.log(data)
-    switch(data.code) {
+    switch (data.code) {
       case 1001:
         alert('该用户已看过')
         console.log('该用户已看过')
@@ -225,28 +273,33 @@ Bling.prototype.render = function () {
       case 0:
         self.data.snap = data.data
         self.loading()
-        // self.showSnap(function () {
-        //   self.showSnapLuck()
-        // })
+        self.showSnap(function() {
+          self.showSnapLuck()
+        })
         break
       default:
         console.log('sys error')
         break
     }
   }, 'json')
+
+  self.bindEvent()
 }
 
-checkLogin()
 var App = new Bling()
-App.init()
+checkLogin(function() {
+  App.init()
+})
 
-$.get(Config.API.getWxSign, function (res) {
-  if(res.code == 0){
+
+$.get(Config.API.getWxSign, function(res) {
+  // alert(JSON.stringify(res))
+  if (res.timestamp) {
     wx.config({
-      debug: false,
+      debug: true,
       appId: 'wx875c7888a7aef3f7',
       timestamp: res.timestamp,
-      nonceStr: res.nonceStr,
+      nonceStr: res.noncestr,
       signature: res.signature,
       jsApiList: [
         'checkJsApi',
@@ -288,81 +341,66 @@ $.get(Config.API.getWxSign, function (res) {
   }
 })
 
-wx.ready(function(){
+wx.error(function(res) {
+  console.log('wx error')
+    // alert(JSON.stringify(res))
+});
+
+wx.ready(function() {
+  // alert('wx.ready')
   Config.isWeixinOk = true
 })
 
-// function checkLogin() {
-//   var bling_uid = getCookie('bling_uid')
-//     , code = getUrlParam('code')
-//   if(!bling_uid){
-//     if(code){
-//       $.get(Config.API.getAuth+code, function (res) {
-//         if(res.code == 0){
-//           setCookie('bling_uid', res.data.uid)
-//           alert(JSON.stringify(res.data))
-//         }
-//       })
-//       return true
-//     }
-//     else{
-//       location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx875c7888a7aef3f7&redirect_uri=http%3A%2F%2Fbling.treedom.cn%2Findex.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
-//     }
-//     return false
-//   }
-//   return true
+function checkLogin(cb) {
+  var bling_uid = getCookie('bling_uid'),
+    code = getUrlParam('code')
+  if (!bling_uid) {
+    if (code) {
+      $.get(Config.API.getAuth + code, function(res) {
+        if (res.code == 0) {
+          setCookie('bling_uid', res.data.uid)
+          // alert(JSON.stringify(res.data))
+          cb && cb()
+        }
+      })
+      return true
+    } else {
+      location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx875c7888a7aef3f7&redirect_uri=http%3A%2F%2Fbling.treedom.cn%2Findex.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
+    }
+    return false
+  }
+  cb && cb()
+  return true
+}
+
+// function checkLogin(cb) {
+//   setCookie('bling_uid', 'o17b6s4BVxHPN5hGdAaTUspsKVC4')
+//   cb && cb()
 // }
 
-function checkLogin() {
-  setCookie('bling_uid', 'o17b6s4BVxHPN5hGdAaTUspsKVC4')
+function setCookie(c_name, value, expiredays) {
+  var exdate = new Date()
+  exdate.setDate(exdate.getDate() + expiredays)
+  document.cookie = c_name + "=" + escape(value) +
+    ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString())
 }
 
-function setCookie(c_name,value,expiredays){
-  var exdate=new Date()
-  exdate.setDate(exdate.getDate()+expiredays)
-  document.cookie=c_name+ "=" +escape(value)+
-  ((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
-}
-
-function getCookie(c_name){
-if (document.cookie.length>0)
-  {
-  c_start=document.cookie.indexOf(c_name + "=")
-  if (c_start!=-1)
-    { 
-    c_start=c_start + c_name.length+1 
-    c_end=document.cookie.indexOf(";",c_start)
-    if (c_end==-1) c_end=document.cookie.length
-    return unescape(document.cookie.substring(c_start,c_end))
-    } 
+function getCookie(c_name) {
+  if (document.cookie.length > 0) {
+    c_start = document.cookie.indexOf(c_name + "=")
+    if (c_start != -1) {
+      c_start = c_start + c_name.length + 1
+      c_end = document.cookie.indexOf(";", c_start)
+      if (c_end == -1) c_end = document.cookie.length
+      return unescape(document.cookie.substring(c_start, c_end))
+    }
   }
-return ""
+  return ""
 }
 
 function getUrlParam(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ nproxy.js
@@ -0,0 +1,8 @@
+module.exports = [
+
+  // 1. replace single file with local one
+  {
+    pattern: 'http://bling.treedom.cn/js/app.js',
+    responder:  "/webDev/git/bling/js/app.js"
+  }
+];
