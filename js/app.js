ab170444a19c89404c97da7497b8cfcf8c2375d3
wheato <wheato2010@gmail.com>
2015-08-30 19:00:23 +0800

    修改中彩蛋的逻辑 增加选择彩蛋类型功能

--- js/app.js
+++ js/app.js
@@ -10,105 +10,130 @@ var Config = {
     getWxSign: 'http://sbk.treedom.cn/ajax/weixin/sign',
     getAuth: 'http://sbk.treedom.cn/ajax/weixin/getAuth/'
   }
-}
-
-//debug
-// Config.API.createSnap = '../test/api/create_snap.json'
-// Config.API.getSnap = 'test/api/get_snap.json'
-// $('body').addClass('bling-start bling-showimg')
-// $('#snapImg').attr('src', 'img/http.jpg')
-// setTimeout(function () {
-//   // $('body').addClass('bling-upload')
-//   $('.page-upload').addClass('page-upload-ready')
-// }, 1000)
-// upload
-// $('body').addClass('bling-start bling-showimg bling-upload')
-$('body').addClass('bling-nolook')
+};
 
 
+$('body').addClass('bling-nolook');
 
 var Bling = function() {
 
-}
+};
 
 Bling.prototype.init = function() {
-  var self = this
+  var self = this;
 
-  this.data = {}
+  this.data = {};
 
-  this.render()
-}
+  this.data.luckyItems = ["挖鼻孔1", "挖鼻孔2", "挖鼻孔3"];
 
+  this.render();
+
+};
+
+//获取URL上的image id
 Bling.prototype.getSnapId = function() {
-  //Todo: get from url
-  return getUrlParam('snapimg')
-}
+
+  return getUrlParam('snapimg');
+
+};
+
+//获取URL上的lucky id
+Bling.prototype.getLuckyId = function(){
+
+  return getUrlParam('luckyid');
+
+};
+
 
 Bling.prototype.showSnap = function(cb) {
   var self = this,
     $body = $('body'),
     snapData = this.data.snap,
-    isHideImg = false
+    isHideImg = false;
 
-  $body.addClass('bling-start')
-    .one('touchstart', function(e) {
-      var autoHideSet = null
+  $body.addClass('bling-start').one('touchstart', function(e) {
 
-      showSnapImg(snapData.snapImgUrl, snapData.snapShowTime)
+      var autoHideSet = null;
+
+      showSnapImg(snapData.snapImgUrl, snapData.snapShowTime);
 
       $body.one('touchend', function(et) {
-        hideSnapImg(cb)
-        et.preventDefault()
-      })
+
+        hideSnapImg(cb);
+
+        et.preventDefault();
+
+      });
 
       autoHideSet = setTimeout(function() {
-        hideSnapImg(cb)
-      }, snapData.snapShowTime * 1000)
 
-      e.preventDefault()
-    })
+        hideSnapImg(cb);
+
+      }, snapData.snapShowTime * 1000);
+
+      e.preventDefault();
+
+    });
 
   function showSnapImg(url, time) {
-    $body.addClass('bling-showimg')
-      .find('#snapImg').attr('src', url)
 
-    self.countDown(1, time * 1000)
-    return true
+    $body.addClass('bling-showimg').find('#snapImg').attr('src', url);
+
+    self.countDown(1, time * 1000);
+
+    return true;
+
   }
 
   function hideSnapImg(cb) {
+
     if (isHideImg) {
-      return
+      return ;
     }
-    $body.addClass('bling-hideimg')
-      // .removeClass('bling-showimg')
 
-    $('#countDown').hide()
-    isHideImg = true
-    cb && cb()
+    $body.addClass('bling-hideimg');
+
+    $('#countDown').hide();
+
+    isHideImg = true;
+
+    alert(snapData.isSnapLuck);
+    if(snapData.isSnapLuck){  //是否中彩蛋
+
+      cb && cb();
+
+    } else {
+
+      $body.removeClass('bling-showimg').removeClass('bling-start');
+      self.render();
+
+    }
+
   }
-}
+};
 
+//中彩蛋展示上传页面
 Bling.prototype.showSnapLuck = function() {
+
   var self = this,
     snapData = this.data.snap;
 
-  // if(!snapData.isSnapLuck){
-  //   return
-  // }
-  $('.snapluck-tips .author').html(snapData.snapAuthor.nickname)
+  $('.snapluck-tips .author').html(snapData.snapAuthor.nickname);
+  $('.snapluck-type').html(data.luckyItems[self.data.currentLucky]); //展示lucky页
 
   $('body').addClass('bling-upload');
 
-  $('.btn-miniabout').on('click', this.showAbout);
+  $('.btn-miniabout').show().on('click', this.showAbout);
+
 
   //动画完成去掉logo
   setTimeout(function(){
     $('.logo-footer').hide();
-  }, 2000);
+  }, 800);
 
 };
 
+//倒计时
 Bling.prototype.countDown = function(percent, time) {
   var canvas = document.getElementById('countDown');
   var ctx = canvas.getContext("2d");
@@ -120,22 +145,22 @@ Bling.prototype.countDown = function(percent, time) {
     dif = 0;
   var loop, re_loop;
   var text, text_w;
-  var timeNum = parseInt(time/1000)
+  var timeNum = parseInt(time/1000);
 
-  $countDown = $('#countDown')
-  $countDown.addClass('countDown-show')
-  draw(percent, time)
-  $('.countDown-wrap .num').html(timeNum)
+  $countDown = $('#countDown');
+  $countDown.addClass('countDown-show');
+  draw(percent, time);
+  $('.countDown-wrap .num').html(timeNum);
   var numT = setInterval(function () {
-    timeNum--
+    timeNum--;
     if(timeNum < 1){
-      $('.countDown-wrap .num').remove()
-      $countDown.remove()
-      clearInterval(numT)
+      $('.countDown-wrap .num').remove();
+      $countDown.remove();
+      clearInterval(numT);
       return;
     }
-    $('.countDown-wrap .num').html(timeNum)
-  }, 1000)
+    $('.countDown-wrap .num').html(timeNum);
+  }, 1000);
 
   function init() {
     // ctx.clearRect(0,0,W,H);
@@ -181,20 +206,21 @@ Bling.prototype.countDown = function(percent, time) {
 }
 
 Bling.prototype.choseImg = function() {
-  var self = this
-    // , imgId = '../img/test.jpg'
 
-  // alert('choseImg')
+  var self = this;
 
   wx.chooseImage({
     success: function(res) {
-      // alert('chose img success')
-      self.data.localImgId = res.localIds
-      $('.page-upload').addClass('page-upload-ready')
+
+      self.data.localImgId = res.localIds;
+      $('.page-upload').addClass('page-upload-ready');
       $('#Preview').attr('src', res.localIds);
 
-      //重新展示logo
-      $('.logo-footer').show();
+      $('.logo-footer').show();   //重新展示logo
+      $('.btn-miniabout').hide();  //隐藏查看About页的按钮
+
+      self.choseLucky();  //选择彩蛋类型
+
     }
   })
 };
@@ -204,46 +230,87 @@ Bling.prototype.loading = function(cb) {
     , $loading = $('#Loading')
     , snap = self.data.snap
     , delay = 0
-    , img = new Image()
+    , img = new Image();
 
-  $('#Loading').show()
+  $('#Loading').show();
+
   if (!(snap.snapImgUrl && /http/.test(snap.snapImgUrl)) && snap.snapImgUrl.length > 4){
-    // alert('begin wx load img')
+
     if(!Config.isWeixinOk){
       delay = 500
     }
+
     setTimeout(function () {
+
       wx.downloadImage({
         serverId: snap.snapImgId,
         isShowProgressTips: 1, // 默认为1，显示进度提示
         success: function (res) {
-          // alert('img load end')
+
           snap.snapImgUrl = res.localId;
           $('#Loading').hide();
-          cb && cb($loading)
+          cb && cb($loading);
+
         }
-      })
-    }, delay)
-  }
-  else {
+      });
+
+    }, delay);
+
+  } else {  //加载自己服务器上的大图
+
     img.onload = function() {
       $('#Loading').hide();
       cb && cb($loading);
     };
+
+    snap.snapImgUrl = Config.cdnDir + snap.snapImgUrl + '.jpg';
     img.src = snap.snapImgUrl
   }
-}
 
+};
+
+//选择埋彩蛋的类型
+Bling.prototype.choseLucky = function(){
+
+  var $select = $('.select-box'),
+      $selectList = $select.find('.snapluck-list'),
+      length = $selectList.find('li').length,
+      current = 0;
+      lock = false;
+
+  $select.on('click', function(){
+    lock = true;
+    this.data.currentLucky = current;
+    next();
+  });
+
+  $selectList.on('webkitTransitionEnd', function(){
+    lock = false;
+  });
+
+  function next(){
+    current ++;
+    if(current == length - 1){
+      current = 0;
+    }
+
+    $selectList.get(0).style.webkitTransform = 'translateY(' + current * 30 + 'px);';
+
+  }
+};
+
+//生成亮瞎图
 Bling.prototype.submitImg = function() {
   var self = this,
-    localImgId
+    localImgId;
 
-  localImgId = self.data.localImgId[0]
+  localImgId = self.data.localImgId[0];
     // alert(localImgId)
   if (!localImgId) {
-    alert('还没选择图片')
+    alert('还没选择图片');
     return false
   }
+
   wx.uploadImage({
     localId: localImgId, // 需要上传的图片的本地ID，由chooseImage接口获得
     isShowProgressTips: 1, // 默认为1，显示进度提示
@@ -252,23 +319,27 @@ Bling.prototype.submitImg = function() {
       $.get(Config.API.createSnap + serverId, function(res) {
         // alert('upload back: '+res.code)
         if (res.code == 0) {
-          self.data.curShareId = serverId
-          self.updateShare()
-          alert('提交成功，可以分享了')
+          self.data.curShareId = serverId;
+          alert('提交成功，可以分享了');
+          self.updateShare();
+          //Todo: 提示分享浮层
         } else {
           alert('提交失败，稍后再试')
         }
       })
     }
-  })
-}
+  });
+};
 
+//设置分享
 Bling.prototype.updateShare = function() {
   var self = this,
     shareUrl,
-    snapThumb = Config.cdnDir + 's_'+ self.data.curShareId +'.jpg'
+    luckyid = self.data.currentLucky,
+    snapThumb = Config.cdnDir + 's_'+ self.data.curShareId +'.jpg';
 
-  shareUrl = Config.site + '?snapimg=' + self.data.curShareId
+  shareUrl = Config.site + '?snapimg=' + self.data.curShareId + '&luckid=' + luckyid;
+
   wx.onMenuShareAppMessage({
     title: 'bling', // 分享标题
     desc: 'bling desc', // 分享描述
@@ -278,24 +349,27 @@ Bling.prototype.updateShare = function() {
     // dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
     success: function() {
       // 用户确认分享后执行的回调函数
-      alert('已分享给朋友')
+      alert('已分享给朋友');
     },
     cancel: function() {
-      alert('取消分享朋友')
+      alert('取消分享朋友');
         // 用户取消分享后执行的回调函数
     }
   });
-}
+};
+
 Bling.prototype.showVisitors = function(data) {
   var self = this
     , vTpl = ''
-    , lTpl = ''
+    , lTpl = '';
 
   if(data.visitors.length){
+
     for(var i = 0; i<data.visitors.length; i++){
       vTpl = vTpl + '<div class="item"><img src="'+data.visitors[i].avatar+'"><span class="user-name">'+data.visitors[i].nickname+'</span></div>'
     }
-    $('#Visitors .userbox-list').html(vTpl)
+
+    $('#Visitors .userbox-list').html(vTpl);
     $('#Visitors').show()
   }
 
@@ -310,13 +384,13 @@ Bling.prototype.showVisitors = function(data) {
 }
 
 Bling.prototype.bindEvent = function() {
-  var self = this
+  var self = this;
 
   $('body')
     .on('touchstart', '[data-upload]', function(e) {
-      self.choseImg()
-      e.preventDefault()
-    })
+      self.choseImg();
+      e.preventDefault();
+    });
 
   $('#submitImg').on('touchstart', function(e) {
     // alert('submitImg')
@@ -326,84 +400,74 @@ Bling.prototype.bindEvent = function() {
 
 };
 
-Bling.prototype.closeAbout = function(){
-  $('.page-about').hide();
-  $('.page-about').off('click', this.closeAbout);
-};
-
 Bling.prototype.showAbout = function(){
-  $('.page-about').show()
-      .on('click', this.closeAbout);
+  var $about = $('.page-about');
+  $about.show();
+  $about.on('click', close);
+  function close(){
+    $about.hide();
+    $about.off('click', close);
+  }
 };
 
 Bling.prototype.render = function() {
   var self = this,
-    curSnapId = this.getSnapId()
+    curSnapId = this.getSnapId();
+
+  self.data.currentLucky = self.getLuckyId();
+  self.data.currentLucky = self.data.currentLucky ? self.data.currentLucky : 0;
 
   if (!curSnapId) {
     return false
   }
 
-// console.log(Config.cdnDir + 's_'+ curSnapId +'.jpg')
-// console.log($('.page-start .cover'))
-//   $('.page-start .cover').css({backgroundImage: 'url('+Config.cdnDir + 's_'+ curSnapId +'.jpg)'})
-  // $('.page-start .cover').css({backgroundImage: test})
-  // console.log($('.page-start .cover').length)
-  // $('.page-start .cover').css({height: 'auto'})
-
   $.get(Config.API.getSnap + curSnapId, function(data) {
-  // $.get(Config.API.getSnap, function(data) {
-    //debug
-    // console.log(data)
-    // data = JSON.parse(data)
-    //debug
-    // self.data.snap = {
-    //   snapImgUrl: 'test'
-    // }
-    // self.showSnap()
-    // self.showSnapLuck()
-    // return;
 
-    console.log(data)
+    console.log(data);
 
-    var snapThumb = Config.cdnDir + 's_'+ curSnapId +'.jpg'
+    var snapThumb = Config.cdnDir + 's_'+ curSnapId +'.jpg';
     // var snapThumb = 'img/test-thumb.jpg'
     switch (data.code) {
+
       case 1001:
         // alert('该用户已看过')
-        $('html').addClass('bling-nolook')
-        $('.nolook-tips').html('你已看过啦，图片已销毁了')
-        self.showVisitors(data.data)
-        console.log('该用户已看过')
-        break
+        $('html').addClass('bling-nolook');
+        $('.nolook-tips').html('你已看过啦，图片已销毁了');
+        self.showVisitors(data.data);
+        console.log('该用户已看过');
+        break;
+
       case 1002:
-        $('html').addClass('bling-nolook')
-        $('.nolook-tips').html('傻眼了吧   来晚啦！')
-        self.showVisitors(data.data)
-        console.log('超过可看人数')
-        break
+        $('html').addClass('bling-nolook');
+        $('.nolook-tips').html('傻眼了吧   来晚啦！');
+        self.showVisitors(data.data);
+        console.log('超过可看人数');
+        break;
+
       case 0:
-        $('.page-start .cover').css({backgroundImage: 'url('+ snapThumb +')'}).show()
-        self.data.snap = data.data
+        $('.page-start .cover').css({backgroundImage: 'url('+ snapThumb +')'}).show();
+        self.data.snap = data.data;
         self.loading(function () {
           self.showSnap(function() {
             self.showSnapLuck()
           })
-        })
-        break
+        });
+        break;
+
       default:
-        console.log('sys error')
+        console.log('sys error');
         break
     }
-  }, 'json')
+  }, 'json');
 
-  self.bindEvent()
-}
+  self.bindEvent();
+};
 
-var App = new Bling()
+var App = new Bling();
+
 checkLogin(function() {
   App.init()
-})
+});
 
 
 $.get(Config.API.getWxSign, function(res) {
@@ -453,7 +517,7 @@ $.get(Config.API.getWxSign, function(res) {
       ]
     })
   }
-})
+});
 
 wx.error(function(res) {
   console.log('wx error')
@@ -463,12 +527,14 @@ wx.error(function(res) {
 wx.ready(function() {
   // alert('wx.ready')
   Config.isWeixinOk = true
-})
+});
 
 function checkLogin(cb) {
+
   var bling_uid = getCookie('bling_uid')
     , code = getUrlParam('code')
-    , param = getUrlParam('snapimg') || ''
+    , param = getUrlParam('snapimg') || '';
+
   if (!bling_uid) {
     if (code) {
       $.get(Config.API.getAuth + code, function(res) {
@@ -477,7 +543,7 @@ function checkLogin(cb) {
             // alert(JSON.stringify(res.data))
           cb && cb()
         }
-      })
+      });
       return true
     } else {
       if(param){
@@ -488,7 +554,7 @@ function checkLogin(cb) {
     }
     return false
   }
-  cb && cb()
+  cb && cb();
   return true
 }
 
